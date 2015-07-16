8894aadf5fb13023c601012411bee3b70ccc492b
Bsdage <bsdage@nix.bra>
2015-07-16 12:03:18 +0800

    add makefile & header file for full version

---
+++ demo/msp430/config-full-without-priority-inversion/femtoRTOSconfig.h
@@ -0,0 +1,5 @@
+#define USE_PORT_MSP430
+//#define TRACE_SWITCH_CONTEXT TRACE
+#define USE_TASK_LISTS
+
+typedef unsigned tick_t;
---
+++ demo/msp430/config-full/femtoRTOSconfig.h
@@ -0,0 +1,6 @@
+#define USE_PORT_MSP430
+//#define TRACE_SWITCH_CONTEXT TRACE
+#define USE_TASK_LISTS
+#define PRIORITY_INVERSION_SUPPORT
+
+typedef unsigned tick_t;
